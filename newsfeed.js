14c7a56fad6367a86d8364919b3a87b301a49ba1
Philip Patton <ppatton35745@gmail.com>
2018-06-06 14:53:24 -0500

    refactored newsfeed functions

--- newsfeed.js
+++ newsfeed.js
@@ -1,5 +1,4 @@
 
-
 const buildNewsFeed = (newsItems) => {
 
     const newsFeedFrag = document.createDocumentFragment();
@@ -11,32 +10,7 @@ const buildNewsFeed = (newsItems) => {
         const newsItemHeader = document.createElement("h2");
         newsItemHeader.textContent = newsItem.heading;
 
-        const newsHighlightsList = document.createElement("ul");
-
-        const newsListItemLikelihood = document.createElement("li");
-        const likelihoodLabel = document.createTextNode("Likelihood: ");
-        const likelihoodValue = document.createTextNode(newsItem.likelihood);
-
-        newsListItemLikelihood.appendChild(likelihoodLabel);
-        newsListItemLikelihood.appendChild(likelihoodValue);
-
-        const newsListItemDate = document.createElement("li");
-        const dateSpan = document.createTextNode("Date: ")
-        const dateValue = document.createTextNode(newsItem.date);
-
-        newsListItemDate.appendChild(dateSpan);
-        newsListItemDate.appendChild(dateValue);
-
-        const newsListItemLocation = document.createElement("li");
-        const whereSpan = document.createTextNode("Where: ");
-        const whereValue = document.createTextNode(newsItem.where);
-
-        newsListItemLocation.appendChild(whereSpan);
-        newsListItemLocation.appendChild(whereValue);
-
-        newsHighlightsList.appendChild(newsListItemLikelihood);
-        newsHighlightsList.appendChild(newsListItemDate);
-        newsHighlightsList.appendChild(newsListItemLocation);
+        const newsHighlightsList = popUL(newsItems);
 
         const synopsisHeader = document.createElement("h3");
         synopsisHeader.textContent = "Synopsis:"
@@ -44,10 +18,10 @@ const buildNewsFeed = (newsItems) => {
         const synopsisParagraph = document.createElement("p");
         synopsisParagraph.textContent = newsItem.synopsis;
 
-        newsArticle.appendChild(newsItemHeader);
-        newsArticle.appendChild(newsHighlightsList);
-        newsArticle.appendChild(synopsisHeader);
-        newsArticle.appendChild(synopsisParagraph);
+        newsArticle.appendChild(newsItemHeader, newsHighlightsList, synopsisHeader, synopsisParagraph);
+        // newsArticle.appendChild(newsHighlightsList);
+        // newsArticle.appendChild(synopsisHeader);
+        // newsArticle.appendChild(synopsisParagraph);
 
         newsFeedFrag.appendChild(newsArticle);
 
@@ -56,3 +30,36 @@ const buildNewsFeed = (newsItems) => {
     return newsFeedFrag
 
 }
+
+const popUL = (newsItems) => {
+    
+    const ul = document.createElement("ul");
+
+    newsItems.forEach(function(newsItem) {
+        
+        for (let prop in newsItem) {
+        
+            if (prop === "likelihood" || prop === "date" || prop === "where") {
+                
+                const li = document.createElement("li");
+            
+                switch (prop) {
+                    case likelihood:
+                        li.appendChild(document.createTextNode("Likelihood: " + newsItem.likelihood))
+                        break;
+                    case date:
+                        li.appendChild(document.createTextNode("Date: " + newsItem.date))
+                        break;
+                    case where:
+                        li.appendChild(document.createTextNode("Where: " + newsItem.where))
+                        break;
+                }
+    
+                ul.appendChild(li);
+            }     
+        }
+    })
+    
+    return ul;
+}
+
