5488dba6b9eac738b7268945fd5e2966a5569cc4
Philip Patton <ppatton35745@gmail.com>
2018-06-06 15:20:13 -0500

    refactored functions and fixed bugs

--- newsfeed.js
+++ newsfeed.js
@@ -1,6 +1,38 @@
+const popUL = (newsItem) => {
+    
+    const ul = document.createElement("ul");
+        
+        for (let prop in newsItem) {
+        
+            if (prop === "likelihood" || prop === "date" || prop === "where") {
+                
+                const li = document.createElement("li");
+            
+                switch (prop) {
+                    case "likelihood":
+                        li.appendChild(document.createTextNode("Likelihood: " + newsItem.likelihood))
+                        break;
+                    case "date":
+                        li.appendChild(document.createTextNode("Date: " + newsItem.date))
+                        break;
+                    case "where":
+                        li.appendChild(document.createTextNode("Where: " + newsItem.where))
+                        break;
+                }
+    
+                ul.appendChild(li);
+            }     
+        }
+    
+    return ul;
+}
+
+
 
 const buildNewsFeed = (newsItems) => {
 
+    const newsFeedSection = document.createElement("section");
+    newsFeedSection.id = "newsFeed";
     const newsFeedFrag = document.createDocumentFragment();
 
     newsItems.forEach(newsItem => {
@@ -10,7 +42,7 @@ const buildNewsFeed = (newsItems) => {
         const newsItemHeader = document.createElement("h2");
         newsItemHeader.textContent = newsItem.heading;
 
-        const newsHighlightsList = popUL(newsItems);
+        const newsHighlightsList = popUL(newsItem);
 
         const synopsisHeader = document.createElement("h3");
         synopsisHeader.textContent = "Synopsis:"
@@ -18,48 +50,19 @@ const buildNewsFeed = (newsItems) => {
         const synopsisParagraph = document.createElement("p");
         synopsisParagraph.textContent = newsItem.synopsis;
 
-        newsArticle.appendChild(newsItemHeader, newsHighlightsList, synopsisHeader, synopsisParagraph);
-        // newsArticle.appendChild(newsHighlightsList);
-        // newsArticle.appendChild(synopsisHeader);
-        // newsArticle.appendChild(synopsisParagraph);
+        newsArticle.appendChild(newsItemHeader);
+        newsArticle.appendChild(newsHighlightsList);
+        newsArticle.appendChild(synopsisHeader);
+        newsArticle.appendChild(synopsisParagraph);
 
-        newsFeedFrag.appendChild(newsArticle);
+        newsFeedSection.appendChild(newsArticle);
 
     });
+
+    newsFeedFrag.appendChild(newsFeedSection);
 
     return newsFeedFrag
 
 }
 
-const popUL = (newsItems) => {
-    
-    const ul = document.createElement("ul");
-
-    newsItems.forEach(function(newsItem) {
-        
-        for (let prop in newsItem) {
-        
-            if (prop === "likelihood" || prop === "date" || prop === "where") {
-                
-                const li = document.createElement("li");
-            
-                switch (prop) {
-                    case likelihood:
-                        li.appendChild(document.createTextNode("Likelihood: " + newsItem.likelihood))
-                        break;
-                    case date:
-                        li.appendChild(document.createTextNode("Date: " + newsItem.date))
-                        break;
-                    case where:
-                        li.appendChild(document.createTextNode("Where: " + newsItem.where))
-                        break;
-                }
-    
-                ul.appendChild(li);
-            }     
-        }
-    })
-    
-    return ul;
-}
 
