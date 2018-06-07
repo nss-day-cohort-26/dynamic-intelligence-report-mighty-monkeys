fd1adb130ec52b55d547b190459b2f88ed91ebc9
Philip Patton <ppatton35745@gmail.com>
2018-06-07 11:48:43 -0500

    add basic styling to news feed section

--- newsfeed.js
+++ newsfeed.js
@@ -10,8 +10,22 @@ const popUL = (newsItem) => {
             
                 switch (prop) {
                     case "likelihood":
-                        li.appendChild(document.createTextNode("Likelihood: " + newsItem.likelihood))
+                        //li.appendChild(document.createTextNode("Likelihood: " + newsItem.likelihood))
+                        for (let i = 0; i < newsItem.likelihood; i++) {
+                            const icon = document.createElement("i");
+                            icon.classList = "fas fa-star";
+                            li.appendChild(icon);
+                        }
+
+                        for (let i = 0; i < (10 - newsItem.likelihood); i++) {
+                            const icon = document.createElement("i");
+                            icon.classList = "far fa-star";
+                            li.appendChild(icon);
+                        }
+
+
                         break;
+
                     case "date":
                         li.appendChild(document.createTextNode("Date: " + newsItem.date))
                         break;
