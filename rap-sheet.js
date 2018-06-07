9c6bbe322cc8b8c2fd19342fde1e7485a4dd5bb9
Sathvik Reddy <sathvik.reddy615@gmail.com>
2018-06-07 10:06:12 -0500

    organized functions and added images to website

--- rap-sheet.js
+++ rap-sheet.js
@@ -2,7 +2,9 @@ const rapSheetFrag = document.createDocumentFragment();
 const sectionRapSheet = document.createElement("section");
 sectionRapSheet.setAttribute("id", "rap-sheet");
 
-// List Arrays
+// let img = document.createElement("img");
+// img.setAttribute("src", "img_pulpit.jpg");
+// img.setAttribute("alt", "NameofImage");
 
 const myLists = {
     headers1: {
@@ -11,21 +13,14 @@ const myLists = {
         header3: "Witnessess",
         header4: "Victims",
     },
+    headers3: {
+        header1: "Committed",
+        header2: "Convicted"
+    },
     headers4: {
         header1: "Guantanamo Bay Prison",
         header2: "Alcatraz Federal Penitentiary",
         header3: "Riverbend Maximum Security",
-    },
-    prison: {
-        img1: "",
-        img2: "",
-        img3: "",
-        img4: "",
-        img5: "",
-        img6: "",
-        img7: "",
-        img8: "",
-        img9: ""
     },
     dates: {
         date1: "2002-2005",
@@ -49,157 +44,138 @@ const myLists = {
     },
     convictedList: {
         list1: "Identity Theft",
-        list2: "Jaywalking",
+        list2: "Grand Theft Auto",
         list3: "Battery"
     }
 }
-      
+
 const crimeSections = () => {
 
+    // Function - creates Headers for each CHILD Container
     const createContainerHeader = ob => {
         const createHeader = document.createElement("h1");
         createHeader.innerHTML = myLists.headers1[ob];
-        return createHeader
+        return createHeader;
     }
 
-    // Crimes Section ----------------------------
-
-    // Crime Header 
-
-    let crimesH1 = createContainerHeader("header1");
-
-    // Committed Child
-
-    const committedContainer = document.createElement("div");
-    committedContainer.setAttribute("id", "committedSection"); 
-    
-    const committedH3 = document.createElement("h3");
-    committedH3.innerHTML = "Committed";
-    
-    const committedUl = document.createElement("ul");
-
-    for (let i in myLists.committedList) {
-        const committedLi = document.createElement("li");
-        committedLi.innerHTML = myLists.committedList[i];
-        committedUl.appendChild(committedLi);
+    // Function - creates PARENT Containers for each Child Container below
+    const createParentContainer = val => {
+        const createParent = document.createElement("div");
+        createParent.id = val;
+        return createParent;
     }
-      
-    committedContainer.appendChild(committedH3);
-    committedContainer.appendChild(committedUl);
 
-    // Convicted Child
-
-    const convictedContainer = document.createElement("div");
-    convictedContainer.setAttribute("id", "convictedSection"); 
-    
-    const convictedH3 = document.createElement("h3");
-    convictedH3.innerHTML = "Committed"
-    
-    const convictedUl = document.createElement("ul");
-
-    for (let i in myLists.convictedList) {
-        const convictedLi = document.createElement("li");
-        convictedLi.innerHTML = myLists.convictedList[i];
-        convictedUl.appendChild(convictedLi);
+    // Function - creates Crime Committed & Convicted CHILD Containers
+    const createCrime = (ob, ob2, ob3) => {
+        const crimeContainer = document.createElement("div");
+        const crimeH3 = document.createElement("h3");
+        crimeH3.innerHTML = myLists[ob][ob2];
+        const crimeUl = document.createElement("ul");
+        for (let i in myLists[ob3]) {
+            const crimeLi = document.createElement("li");
+            crimeLi.innerHTML = myLists[ob3][i];
+            crimeUl.appendChild(crimeLi);
+        }
+        crimeContainer.appendChild(crimeH3);
+        crimeContainer.appendChild(crimeUl);
+        return crimeContainer;
     }
- 
-    convictedContainer.appendChild(convictedH3);
-    convictedContainer.appendChild(convictedUl);
 
-    // Parent Container
-    const crimeSection = document.createElement("div"); 
-    crimeSection.setAttribute("id", "crimeSection"); 
-    crimeSection.appendChild(committedContainer);
-    crimeSection.appendChild(convictedContainer);
-
-    // Prison Section --------------------------------
-
-    // Prison Header 
-    let prisonH1 = createContainerHeader("header2");
-
-    // Create Prison Container
-
-    const createPrison = (ob, ob2) => {
+    // Function - creates Prison CHILD Container
+    const createPrison = (ob, ob2, ob3) => {
         const prisonContainer = document.createElement("div");
         const prisonH4 = document.createElement("h4");
         prisonH4.innerHTML = myLists.headers4[ob];
+        const prisonImg = document.createElement("img");
+        prisonImg.setAttribute("src", ob2);
         const prisonP = document.createElement("p");
-        prisonP.innerHTML = myLists.dates[ob2];
+        prisonP.innerHTML = myLists.dates[ob3];
         prisonContainer.appendChild(prisonH4);
+        prisonContainer.appendChild(prisonImg);
         prisonContainer.appendChild(prisonP);
         return prisonContainer;
     }
 
-    let prison1 = createPrison("header1", "date1");
-    let prison2 = createPrison("header2", "date2");
-    let prison3 = createPrison("header3", "date3");
+    // Function - creates Victim & Witness CHILD Containers
+    const createPeople = (ob, ob2, ob3) => {
+        const peopleContainer = document.createElement("div");
+        const peopleImg = document.createElement("img");
+        peopleImg.setAttribute("src", ob3);
+        const peopleH5 = document.createElement("h5");
+        peopleH5.innerHTML = myLists[ob][ob2];
+        peopleContainer.appendChild(peopleImg);
+        peopleContainer.appendChild(peopleH5);
+        return peopleContainer;
+    }
 
-     // Prison Parent container
-     const prisonParentContainer = document.createElement("div");
-     prisonParentContainer.setAttribute("id", "prison-container");
+    // Crime Container ---------->
 
-     prisonParentContainer.appendChild(prison1);
-     prisonParentContainer.appendChild(prison2);
-     prisonParentContainer.appendChild(prison3);
+    let crimesH1 = createContainerHeader("header1");
 
-    // Witnesses Section ------
+    let crimesCommitted = createCrime("headers3", "header1", "committedList");
+
+    let crimesConvicted = createCrime("headers3", "header2", "convictedList");
+
+    const crimeParent = createParentContainer("crimeParent");
+
+    crimeParent.appendChild(crimesCommitted);
+    crimeParent.appendChild(crimesConvicted);
+
+    // Prison Container ---------->
+
+    let prisonH1 = createContainerHeader("header2");
+
+    let prison1 = createPrison("header1", "sr-images/prison.jpg", "date1");
+    let prison2 = createPrison("header2", "sr-images/prison2.jpg", "date2");
+    let prison3 = createPrison("header3", "sr-images/prison3.jpg", "date3");
+
+    const prisonParent = createParentContainer("prisonParent");
+    
+    prisonParent.appendChild(prison1);
+    prisonParent.appendChild(prison2);
+    prisonParent.appendChild(prison3);
+
+    // Witness Container ---------->
 
     let witnessH1 = createContainerHeader("header3");
 
-    const createWitness = (ob, ob2) => {
-        const witnessContainer = document.createElement("div");
-        const witnessH5 = document.createElement("h5");
-        witnessH5.innerHTML = myLists.witnessess[ob];
-        witnessContainer.appendChild(witnessH5);
-        return witnessContainer;
-    }
+    let witness1 = createPeople("witnessess", "witness1", "sr-images/Ron_Swanson.jpg");
+    let witness2 = createPeople("witnessess", "witness2", "sr-images/Tom_Haverford.jpg");
+    let witness3 = createPeople("witnessess", "witness3", "sr-images/Jerry_Gergich.jpg");
 
-    let witness1 = createWitness("witness1");
-    let witness2 = createWitness("witness2");
-    let witness3 = createWitness("witness3");
+    const witnessParent = createParentContainer("witnessParent");
 
-    const witnessParentContainer = document.createElement("div");
-    witnessParentContainer.setAttribute("id", "witness-container");
+    witnessParent.appendChild(witness1);
+    witnessParent.appendChild(witness2);
+    witnessParent.appendChild(witness3);
 
-    witnessParentContainer.appendChild(witness1);
-    witnessParentContainer.appendChild(witness2);
-    witnessParentContainer.appendChild(witness3);
-
-    // Victims Section ------
+    // Victim Container ----------> 
 
     let victimH1 = createContainerHeader("header4");
 
-    const createVictim = ob => {
-        const victimContainer = document.createElement("div");
-        const victimH5 = document.createElement("h5");
-        victimH5.innerHTML = myLists.victims[ob];
-        victimContainer.appendChild(victimH5);
-        return victimContainer;
-    }
+    let victim1 = createPeople("victims", "victim1", "sr-images/Chris_Traeger.jpg");
+    let victim2 = createPeople("victims", "victim2", "sr-images/leslie_knope.png");
+    let victim3 = createPeople("victims", "victim3", "sr-images/Ann_Perkins.jpg");
 
-    let victim1 = createVictim("victim1");
-    let victim2 = createVictim("victim2");
-    let victim3 = createVictim("victim3");
+    const victimParent = createParentContainer("victimParent");
 
-    const victimParentContainer = document.createElement("div");
-    victimParentContainer.setAttribute("id", "victim-container");
+    victimParent.appendChild(victim1);
+    victimParent.appendChild(victim2);
+    victimParent.appendChild(victim3);
 
-    victimParentContainer.appendChild(victim1);
-    victimParentContainer.appendChild(victim2);
-    victimParentContainer.appendChild(victim3);
+    // Append to Section Element 
 
     sectionRapSheet.appendChild(crimesH1);
-    sectionRapSheet.appendChild(crimeSection);
+    sectionRapSheet.appendChild(crimeParent);
     sectionRapSheet.appendChild(prisonH1);
-    sectionRapSheet.appendChild(prisonParentContainer);
+    sectionRapSheet.appendChild(prisonParent);
     sectionRapSheet.appendChild(witnessH1);
-    sectionRapSheet.appendChild(witnessParentContainer);
+    sectionRapSheet.appendChild(witnessParent);
     sectionRapSheet.appendChild(victimH1);
-    sectionRapSheet.appendChild(victimParentContainer);
+    sectionRapSheet.appendChild(victimParent);
 
 }
 
 rapSheetFrag.appendChild(sectionRapSheet);
 wrapper.appendChild(rapSheetFrag);
 crimeSections();
-
