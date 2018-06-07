4d907714add5d87160fc061f37dc65b8387c3691
Jordan <jordanwilliams288@gmail.com>
2018-06-06 21:29:07 -0500

    added navbar

--- DOMController.js
+++ DOMController.js
@@ -1,9 +1,13 @@
+const bodyRef = document.querySelector("body");
 const wrapper = document.querySelector("#wrapper");
 const fragment = document.createDocumentFragment();
 
+bodyRef.insertBefore(navbarFrag, wrapper);
+// bodyRef.appendChild(navbarFrag);
+// fragment.appendChild(navbarFrag);
 fragment.appendChild(homeFrag);
 fragment.appendChild(rapSheetFrag);
-fragment.appendChild(buildNewsFeed(newsItems));
+//fragment.appendChild(buildNewsFeed(newsItems));
 fragment.appendChild(financeFrag);
 fragment.appendChild(travelFrag);
 wrapper.appendChild(fragment);
---
+++ navbar.js
@@ -0,0 +1,41 @@
+let navbarFrag = document.createDocumentFragment();
+
+const divRef = document.createElement("div");
+const aRefHome = document.createElement("a");
+const aRefRapSheet = document.createElement("a");
+const aRefNewsFeed = document.createElement("a");
+const aRefFinance = document.createElement("a");
+const aRefTravel = document.createElement("a");
+aRefHome.href = "#home"
+aRefRapSheet.href = "#rap-sheet"
+aRefNewsFeed.href = "#"
+aRefFinance.href = "#finance"
+aRefTravel.href = "#section-Travel"
+aRefHome.textContent = "Home"
+aRefRapSheet.textContent = "Rap Sheet"
+aRefNewsFeed.textContent = "News Feed"
+aRefFinance.textContent = "Finance"
+aRefTravel.textContent = "Travel"
+
+divRef.appendChild(aRefHome);
+divRef.appendChild(aRefRapSheet);
+divRef.appendChild(aRefNewsFeed);
+divRef.appendChild(aRefFinance);
+divRef.appendChild(aRefTravel);
+navbarFrag.appendChild(divRef);
+
+{/* <div class="sidenav">
+<a href="#executive">Suspect</a>
+<a href="#rap">Rap Sheet</a>
+<a href="#news">News</a>
+<a href="#finance">Finance</a>
+<a href="#travel">Travel</a>
+</div> */}
+
+
+
+
+
+
+//write navbar outside of article tag
+
