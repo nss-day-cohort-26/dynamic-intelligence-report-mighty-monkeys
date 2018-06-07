7b70c780157f2bf9e23c24ba703e7fe4a662f073
Joey Smith <smithjoey5123@gmail.com>
2018-06-07 10:36:15 -0500

    need new code

--- travel.js
+++ travel.js
@@ -11,7 +11,8 @@ let travelFrag = document.createDocumentFragment()
 //functions
 const onload =(array,h3text,imageName)=>{ //takes two arguments, array and header text
     const ulRef = document.createElement("ul") // create ul to hold li items
-    const h3Ref =document.createElement("h3") // create tag h3
+    const h3Ref =document.createElement("h3") 
+    h3Ref.id="word"// create tag h3
     const h3Img = document.createElement("IMG");
     h3Img.src = `images/${imageName}`
     artRef.appendChild(secID) //artRef is html class wrapper and add my new section Id
@@ -19,7 +20,7 @@ const onload =(array,h3text,imageName)=>{ //takes two arguments, array and heade
     newH3 = document.createTextNode(h3text); //add argument to text node
     h3Ref.appendChild(newH3) //append h3 content to h3
     secID.appendChild(h3Ref) //append h3 to section
-    h3Ref.appendChild(h3Img) //append img to h3
+    // h3Ref.appendChild(h3Img) //append img to h3
     
     for (let i = 0; i < array.length;i++){ //for loop to access arrays and add to ul
         const liRef = document.createElement("li")
